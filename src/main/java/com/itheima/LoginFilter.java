213311ab6a78bd8dc3fedaa3c67e93babe0237c9
k503394469 <k503394469@gmail.com>
2019-12-09 23:50:01 +0900

    Linux

--- src/main/java/com/itheima/LoginFilter.java
+++ src/main/java/com/itheima/LoginFilter.java
@@ -11,22 +11,14 @@ public class LoginFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        HttpServletRequest request= (HttpServletRequest) req;
-        HttpServletResponse response= (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
         Object userInfo = request.getSession().getAttribute("userInfo");
-        String uri = request.getRequestURI();
-        if (uri.contains(".js")||uri.contains("login.jsp")||uri.contains("menu_search.jsp")||uri.contains("*.png")||uri.contains("*.jpg")
-                ||uri.contains("*.gif")||uri.contains("*.css")||uri.contains("head.jsp")||uri.contains("foot.jsp")||uri.contains("/user/checkName")
-        ||uri.contains("/user/login")){
-            chain.doFilter(request,response);
-        }else {
-            if (userInfo==null||"".equals(userInfo)){
-                request.setAttribute("error_msg","please sign in");
-                request.getRequestDispatcher("/login.jsp").forward(request,response);
-                return;
-            }else {
-                chain.doFilter(request,response);
-            }
+        if (userInfo == null || "".equals(userInfo)) {
+            response.sendRedirect(request.getContextPath()+"/login.jsp");
+            return;
+        } else {
+            chain.doFilter(request, response);
         }
     }
 
